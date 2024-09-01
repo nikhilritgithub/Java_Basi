a58fa0ea1000ca9e02e35edafd12819ea74e77bf
Nikhil kumar <142686565+nikhilritgithub@users.noreply.github.com>
2024-09-01 15:01:39 +0530

    sum of arr

---
+++ arrsum.java
@@ -0,0 +1,12 @@
+public class arrsum {
+
+    
+    public static void main(String args[]){
+        int arr[]= {30,20,20};
+        int sum =0;
+        for(int i=0; i<arr.length; i++){
+            sum += arr[i];
+        }
+        System.out.println(sum);
+    }
+}
