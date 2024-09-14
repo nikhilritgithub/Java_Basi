e81c60240e33e0874b738c939bba31ba0ccdf5ab
Nikhil kumar <142686565+nikhilritgithub@users.noreply.github.com>
2024-09-14 12:17:48 +0530

    find subarray

---
+++ subarray.java
@@ -0,0 +1,37 @@
+// 2 
+// 2 4 
+// 2 4 6 
+// 2 4 6 8 
+// 2 4 6 8 10 
+
+// 4
+// 4 6
+// 4 6 8
+// 4 6 8 10
+
+// 6
+// 6 8
+// 6 8 10
+
+// 8
+// 8 10
+
+// 10
+
+public class subarray {
+    public static void SubArray(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            for(int j=i; j<arr.length; j++){    
+                for(int k=i; k<=j; k++){
+                    System.out.print(arr[k] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        int arr[] = {2,4,6,8,10};
+        SubArray(arr);
+    }
+}
