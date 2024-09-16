df4dd55d56a038b82e460144e5ddd189437703bd
Nikhil kumar <142686565+nikhilritgithub@users.noreply.github.com>
2024-09-16 14:05:34 +0530

    String find

---
+++ firststring.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class firststring {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+    //     String name;
+    //     System.out.println("ENter your name");
+    //      name = sc.next();
+    //     name = sc.nextLine();
+    //     System.out.println(name);
+
+        String Myname = "Nikhil Mehta";
+        System.out.println(Myname.length());
+        String first = "Virat";
+        String last = "kohli";
+        String Full = first + " " + last;
+        System.out.println(Full);
+        System.out.println(Full.length());
+      
+    }
+}
