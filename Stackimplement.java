7067ab424f799dea42b8c55d1249ce45676af063
Nikhil kumar <142686565+nikhilritgithub@users.noreply.github.com>
2024-11-06 20:03:19 +0530

    stack implention

---
+++ Stackimplement.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class Stackimplement {
+    static class Stack{
+        static ArrayList<Integer> list = new ArrayList<>();
+        
+        //function to check stack is empty or not
+        public static boolean isEmpty(){
+            return list.size() == 0;
+        }
+
+        //push..
+        public static void push(int data){
+            list.add(data);
+        }
+
+        //pop..
+        public static int pop(){
+            int top = list.get(list.size()-1);
+            list.remove(list.size()-1);
+            return top;
+        }
+
+        //peek..
+        public static int peek(){
+            return list.get(list.size()-1);
+        }
+    }
+    public static void main(String args[]){
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+
+    }
+}
