4f59f30f7827e7073f5be35ef802399bd95f58c7
Nikhil kumar <142686565+nikhilritgithub@users.noreply.github.com>
2024-11-15 18:36:59 +0530

    star pattern

--- star.java
+++ star.java
@@ -1,16 +1,60 @@
-import java.util.*;
 public class star {
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter a number :");
-        int n = sc.nextInt();
-
+    public static void halftriangle(int n){
         for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void ultatriangle(int n){
+        for(int i=0; i<=n ; i++){
+            for(int j=1; j<=(n-i); j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void number(int n){
+        for(int i=1; i<=n ; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void spaceHalf(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=(n-i); j++){
+                System.out.print(" ");
+            }
             for(int j=1; j<=i; j++){
                 System.out.print("*");
             }
             System.out.println();
         }
+    }
+
+    public static void flyodstriangle(int n){
+        int count = 1;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(count+" ");
+                count++;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+       halftriangle(5);
+       ultatriangle(5);
+       number(5);
+       spaceHalf(5);
+       flyodstriangle(5);
     }
 }
