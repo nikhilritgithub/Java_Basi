491c0830588f1a0f8a8066693b6f838197977d66
Nikhil kumar <142686565+nikhilritgithub@users.noreply.github.com>
2025-02-11 09:18:25 +0530

    Reverse of an array

--- reverse.java
+++ reverse.java
@@ -1,14 +1,33 @@
-import java.util.*;
 public class reverse {
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter a number:  ");
-        int x = sc.nextInt();
-        while( x>0 ){
-            int lastDigit = x % 10;
-            System.out.print(lastDigit);
-            x = x/10;
+    public static int reverse(int n){
+        while(n>0){
+            int ld = n%10;
+            System.out.print(ld);
+            n = n/10;
         }
+        return n;
+    }
+
+
+    public static int fact(int n){
+        int Fact = 1;
+        for(int i=1; i<=n; i++){
+            Fact *= i;
+        }
+        return Fact;
+    }
+
+    public static void table(int n){
+        for(int i=1; i<=n; i++){
+            System.out.println(n + "*" + i + "=" + n*i);
+        }
+    }
+
+
+    public static void main(String args[]){
+        int num = 3456;
+        reverse(num);
+        System.out.println(fact(5));
+        table(10);
     }
 }
