281e3a6f60eca88aed283959c4f07ccb8583b78d
Nikhil kumar <142686565+nikhilritgithub@users.noreply.github.com>
2025-03-03 20:45:11 +0530

    Quick sort

--- quick.java
+++ quick.java
@@ -1,40 +1,40 @@
 public class quick {
+
     public static void print(int arr[]){
         for(int i=0; i<arr.length; i++){
-            System.out.print(arr[i] + " ");
+            System.out.print(arr[i]+ " ");
         }
     }
 
-   public static void quickSort(int arr[], int start, int end){
-    if(start < end){
-        int pivot = partition(arr, start, end);
-        quickSort(arr, start, pivot-1);
-        quickSort(arr, pivot+1, end);
-    }
-   }
-
-   public static int partition(int arr[], int start, int end){
-    int pivelem = arr[end];
-    int i = start-1;
-
-    for(int j=start; j<end; j++){
-        if(arr[j] < arr[end]){
-            i++;
-            //swap
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
+    public static void quickSort(int arr[], int si, int ei){
+        if(si < ei){
+            int pivot = partition(arr,si,ei);
+            quickSort(arr, si, pivot-1);
+            quickSort(arr, pivot+1, ei);
         }
     }
-    i++;
-    int temp =arr[i];
-    arr[i] = arr[end];
-    arr[end] = temp;
-    return i;
-   }
 
+    public static int partition(int arr[], int si, int ei){
+        int pivelem = arr[ei];
+        int i = si-1;
+
+        for(int j=si; j<ei; j++){
+            if(arr[j] < pivelem){
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        i++;
+        int temp = arr[i];
+        arr[i] = arr[ei];
+        arr[ei] = temp;
+
+        return i;
+    }
     public static void main(String args[]){
-        int arr[] = {6,3,9,5,2,8};
+        int arr[] = {8,3,2,9,1,6,7};
         quickSort(arr, 0, arr.length-1);
         print(arr);
     }
