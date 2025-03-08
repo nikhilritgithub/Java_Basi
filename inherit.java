b290cdd2c1747bdeea88458afb463b7428fab1b2
Nikhil kumar <142686565+nikhilritgithub@users.noreply.github.com>
2025-03-08 13:12:39 +0530

    Inheritance

--- inherit.java
+++ inherit.java
@@ -1,43 +1,28 @@
 public class inherit {
     public static void main(String args[]){
-        Animal a = new Animal();
-        a.eat();
-
-        Cat bs = new Cat();
-        bs.sleep();
-        bs.weight = 4;
-        System.out.println(bs.weight);
-
-        Dog d1 = new Dog();
-        d1.run();
-        d1.breed = "labrador";
-        System.out.println(d1.breed);
+        Fish dolphin = new Fish();
+        dolphin.eat();
+        dolphin.swim();
     }
 }
 
 class Animal{
     String color;
-
     void eat(){
-        System.out.println("eating...");
+        System.out.println("eating....");
     }
 
-    void sleep(){
-        System.out.println("Sleeping...");
+    void breathe(){
+        System.out.println("Breathing.....");
     }
+
+
 }
 
-class Cat extends Animal{
-    int weight;
+class Fish extends Animal{
+    int fins;
 
-    void run(){
-        System.out.println("Running");
-    }
-}
-
-class Dog extends Cat {
-    String breed;
-    void bark(){
-        System.out.println("barking");
+    void swim(){
+        System.out.println("swim in water");
     }
 }
