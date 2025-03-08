9ed7ac7955980eefdc1130841b44975a27d620e1
Nikhil kumar <142686565+nikhilritgithub@users.noreply.github.com>
2025-03-08 13:09:07 +0530

    Variable Arguments in java

---
+++ varArgs.java
@@ -0,0 +1,29 @@
+public class varArgs {
+
+    public static int sum(int... a){
+        int sum = 0;
+        for(int i : a){
+            sum += i;
+        }
+        return sum;
+    }
+
+public static void sentence(String ...elements){
+    for(String element : elements){
+        System.out.printf(element+ " ");
+    }
+    System.out.println();
+}
+
+
+    public static void main(String args[]){
+        System.out.println(sum(3,4));
+        System.out.println(sum(3,4,5));
+        System.out.println(sum(3,4,6));
+
+        sentence("one", "two", "three");
+        sentence("Virat", "kohli");
+        sentence("He", "is", "a", "great","man");
+
+    }
+}
