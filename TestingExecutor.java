22b5c00512dc29415bf7112285aa526756728d64
Nikhil kumar <142686565+nikhilritgithub@users.noreply.github.com>
2025-03-24 11:47:48 +0530

    Executor testing

---
+++ TestingExecutor.java
@@ -0,0 +1,10 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class TestingExecutor {
+    public static void main(String args[]){
+        ExecutorService service = Executors.newSingleThreadExecutor();
+
+        
+    }
+}
