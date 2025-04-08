cf010db8d07820e3d7ffcac8d125221126310e4c
Nikhil kumar <142686565+nikhilritgithub@users.noreply.github.com>
2025-04-08 15:57:16 +0530

    Kadens algo

--- kadens.java
+++ kadens.java
@@ -27,6 +27,26 @@ public class kadens {
            }
            return ms;
         }
-    }
+
+
+
+
+    int maxSubarraySum2(int[] arr) {
+        if (arr.length == 0) {
+         return -1;
+     }
+
+     int maxCurrent = arr[0];
+     int maxGlobal = arr[0];
+
+     for (int i = 1; i < arr.length; i++) {
+         maxCurrent = Math.max(arr[i], maxCurrent + arr[i]);
+         maxGlobal = Math.max(maxGlobal, maxCurrent);
+     }
+
+     return maxGlobal;
+     
+ }
     
+}
 }
