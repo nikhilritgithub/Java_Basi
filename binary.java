0b77b322cb09e94ee11cff655d369aa9b53c3e9d
Nikhil kumar <142686565+nikhilritgithub@users.noreply.github.com>
2025-04-09 16:59:20 +0530

    Binary Search

--- binary.java
+++ binary.java
@@ -1,14 +1,16 @@
 public class binary {
 
-    public static int search(int arr[], int key){
+    public static int BinarSearch(int nums[], int key){
         int start = 0;
-        int end = arr.length-1;
-        while(start <=end){
-            int mid = (start + end)/2;
-            if(arr[mid] == key){
+        int end = nums.length-1;
+
+        while(start<=end){
+            int mid = (start + end) / 2;
+
+            if(nums[mid] == key){
                 return mid;
             }
-            else if(arr[mid] > key){
+            if(nums[mid]> key){
                 end = mid-1;
             }
             else{
@@ -17,38 +19,12 @@ public class binary {
         }
         return -1;
     }
-
-
-
-    public static void reverse(int arr[]){
-        int start = 0, end = arr.length-1;
-        while(start < end){
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-
-            start ++ ;
-            end --;
-
-        }
-    }
-
-    public static void Printarray(int arr[]){
-        for(int i=0; i<arr.length; i++){
-            System.out.print(arr[i] + " ");
-        }
-    }
-
-
-
     public static void main(String args[]){
-     int arr[] = {2,3,4,5,6,7,8,9};
-     int key = 6;
-     int index = search(arr, key);
-     //System.out.println("elemnt found at: " + index);
 
-     reverse(arr);
-     Printarray(arr);
-
+        int arr[] = {2,4,6,8,9,12,14,18};
+        int key = 14;
+    
+        int found = BinarSearch(arr, key);
+        System.out.println("Element found at index: " + found);
     }
 }
