693716e2e690ca2cb5e5ffc9c2858fd3d2ace3f9
Nikhil kumar <142686565+nikhilritgithub@users.noreply.github.com>
2025-05-12 20:16:41 +0530

    String Operations

--- strings.java
+++ strings.java
@@ -1,33 +1,42 @@
 import java.util.*;
 public class strings {
 
-    public static void printLetters(String str){
-        for(int i=0; i<str.length(); i++){
-            System.out.print(str.charAt(i)+ " ");
+    public static boolean isPalindrome(String str){
+        int n = str.length();
+        for(int i=0; i<str.length()/2; i++){
+            if(str.charAt(i) != str.charAt(n-1-i)){
+                return false;
+            }
         }
+        return true;
     }
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        // System.out.println("Enter your name");
-        // String name = sc.next();
-        // System.out.println("your name is: "+name);
-
-        // System.out.println(name.length());
 
 
+    public static String substring(String str, int si, int ei){
+        String substr = "";
+        for(int i=si; i<ei; i++){
+            substr += str.charAt(i);
+        }
+        return substr;
+    }
 
-        // System.out.println("Enter first name");
-        // String firstName = sc.next();
-        // System.out.println("Enter last name");
-        // String lastName = sc.next();
-        
-        // String fullName = firstName +" "+ lastName;
-        // System.out.println("your name is: " + fullName);
 
-        // System.out.println(fullName.length());
+    public static int countvowel(String str){
+        int count = 0;
+        for(int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+                count++;
+            }
+        }
+        return count;
+    }
 
-        // System.out.println(fullName.charAt(4));
+    public static void main(String[] args) {
+        String str = "racecars";
+        System.out.println(isPalindrome(str));
+        System.out.println(substring(str, 0, 3));
 
-        printLetters("Virat kohli");
+        System.out.println("vowel Count is: " + countvowel(str));
     }
 }
